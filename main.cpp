f35a8795b892af824cdfba83e63a0e612ef88a2d
LorisPanaroGit <lorispanaro28@gmail.com>
2023-12-31 18:59:35 +0100

    Drawn a rectangle inside a window context

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 #include <glut.h>
 #include <GLFW/glfw3.h>
 
+void draw_rect(GLFWwindow *win) {
+    /*glClear -> clears the color bit buffer*/
+    glClear(GL_COLOR_BUFFER_BIT);
+    /*glColor3f -> set the color of the buffer*/
+    glColor3f(0.5, 0.7, 0.3);
+    /*glBegin -> specifies the geometry type of the incoming vertices (deprecated: learn VBO and VAO)*/
+    glBegin(GL_QUADS);
+    glVertex2f(-0.5f, -0.5f); // Bottom-left vertex
+    glVertex2f(0.5f, -0.5f);  // Bottom-right vertex
+    glVertex2f(0.5f, 0.5f);   // Top-right vertex
+    glVertex2f(-0.5f, 0.5f);  // Top-left vertex
+    glEnd();
+    /*glfwSwapBuffers -> places the window behing the rectangle, otherwise we don't see any rectangle*/
+    glfwSwapBuffers(win);
+
+}
+
 int main()
 {
     GLFWwindow *window;
@@ -18,6 +35,7 @@ int main()
     }
     glfwMakeContextCurrent(window);
     while(!glfwWindowShouldClose(window)) {
+        draw_rect(window);
         glfwPollEvents();
     }
     glfwDestroyWindow(window);
